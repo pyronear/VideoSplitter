ff01e4c96d74021ab47d73645bf14a3fa96dea7a
Bruno Lenzi <Bruno.Lenzi@cern.ch>
2019-12-13 09:13:59 +0100

    refactor: remove method bestSets, functionality now included in getSets

--- prepareDataset.py
+++ prepareDataset.py
@@ -4,47 +4,21 @@ import pandas as pd
 import numpy as np
 from parseAnnotations import getFrameLabels, extractFrames
 
-
-def getSets(labels, choices=['train', 'valid', 'test'],
-            p=[0.7, 0.2, 0.1], groupby='fBase'):
+def getSets(labels, choices=['train', 'valid', 'test'], p=[0.7, 0.2, 0.1],
+            groupby='fBase', max_attempts=100, tolerance=0.01):
     """
     Assign each row of the given DataFrame to sets like 'training', 'valid', 'test'.
     The set is chosen randomly for each group (e.g. each movie) according to the given
     probabilities.
 
-    Args:
-    - labels: DataFrame containing frame labels (see parseAnnotations.getFrameLabels)
-    - choices: list, default: ['train', 'valid', 'test']
-    - p: list, default: [0.7, 0.2, 0.1]. Probability of each set passed to
-      numpy.random.choice
-    - groupby: str or list of columns in DataFrame labels, default: 'fBase'.
-      Used to define the groups
-
-    Return:
-    - DataFrame with extra column 'dest', with values like 'train', 'valid', 'test'
-    """
-    assert len(choices) == len(p), f'choices and probabilities (p) must have the same length, got {choices}, {p}'
-
-    def fcn(x):
-        return np.random.choice(choices, p=p)
-
-    dest = labels.groupby(groupby).apply(fcn).rename('dest')
-    return pd.merge(dest, labels, left_index=True, right_on=groupby)
-
-
-def bestSets(labels, choices=['train', 'valid', 'test'],
-             p=[0.7, 0.2, 0.1], groupby='fBase', max_attempts=100, tolerance=0.01):
-    """
-    Call getSets until the number of elements in each set matches the
-    desired probabilities within the given tolerance.
-
-    After max_attempts, returns the choice that minimises the sum of the differences
-    between actual and desired probabilities
-
-    N.B.: the number of elements per group may vary substantially so depending on the
+    The number of elements per group may vary substantially so depending on the
     assignment of each group into e.g. 'training', 'valid' and 'test' sets, the sets may
     be populated with different probabilities compared to the groups.
 
+    To try to match the desired probabilities within the given tolerance, the assignment
+    is done a few times. After max_attempts, returns the choice that minimises the sum
+    of the differences between actual and desired probabilities.
+
     Args:
     - labels: DataFrame containing frame labels (see parseAnnotations.getFrameLabels)
     - choices: list, default: ['train', 'valid', 'test']
@@ -52,24 +26,30 @@ def bestSets(labels, choices=['train', 'valid', 'test'],
       numpy.random.choice
     - groupby: str or list of columns in DataFrame labels, default: 'fBase'.
       Used to define the groups
-    - max_attempts: int, default: 100. Maximum calls to getSets
+    - max_attempts: int, default: 100. Maximum number of attempts
     - tolerance: float or list, default: 0.01. Maximum difference between the actual
       probability and the desired one to stop the iteration before max_attempts
 
     Returns:
     - DataFrame with extra column 'dest', with values 'train', 'valid' or 'test'
     """
+    assert len(choices) == len(p), f'choices and probabilities (p) must have the same length, got {choices}, {p}'
+
+    def fcn(x):
+        return np.random.choice(choices, p=p)
+
     def distance(dest):
         "Distance between the desired and actual probabilities for each set"
         return abs(dest.value_counts(normalize=True).reindex(choices).fillna(0) - p)
 
     def loop():
         """
-        Yield the result of getSets and stop if the probabilities match to
+        Yield the resulting DataFrame and stop if the probabilities match to
         better than the desired tolerance
         """
         for _ in range(max_attempts):
-            new_labels = getSets(labels, choices, p, groupby)
+            dest = labels.groupby(groupby).apply(fcn).rename('dest')
+            new_labels = pd.merge(dest, labels, left_index=True, right_on=groupby)
             yield new_labels
             if all(distance(new_labels.dest) < tolerance):
                 return
--- test/test_prepareDataset.py
+++ test/test_prepareDataset.py
@@ -8,7 +8,7 @@ class PrepareDatasetTester(unittest.TestCase):
     """
     Test functions in prepareDataset
     """
-    def test_getSets(self):
+    def test_getSets1(self):
         a = pd.DataFrame({'fBase': range(1000)})
         x = prepareDataset.getSets(a)
         self.assertEqual(x.shape, (1000, 2))
@@ -19,17 +19,17 @@ class PrepareDatasetTester(unittest.TestCase):
         x = prepareDataset.getSets(a, groupby=['a', 'b'], choices=[0, 1], p=[0.3, 0.7])
         self.assertEqual(x.shape, (1000, 3))
 
-    def test_bestSets(self):
+    def test_getSets3(self):
         "Test probabilities in bestSets to 1% in inhomogeneous groups"
         p = [0.4, 0.3, 0.2, 0.1]
         choices = [0, 1, 2, 3]
 
         # Generate
-        n = np.random.choice([2, 4, 6], size=50)
+        n = np.random.choice([2, 4, 6], size=100)
         a = pd.DataFrame({'fBase': np.repeat(range(len(n)), n)})
 
         # Assert
-        b = prepareDataset.bestSets(a, choices=choices, p=p)
+        b = prepareDataset.getSets(a, choices=choices, p=p)
         for i, j in zip(choices, p):
             np.testing.assert_almost_equal((b.dest == i).mean(), j, decimal=2)
 
