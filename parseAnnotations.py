e215ead117d6095ade103735ad62a6fed9998416
Akilditu <sebelm@hotmail.com>
2020-04-03 14:10:53 +0200

    fix: issues in JSON parsing for z column and standardization of all csv files

--- parseAnnotations.py
+++ parseAnnotations.py
@@ -213,15 +213,22 @@ class AnnotationParser:
 
         DFS = [
             d['av'].apply(splitKeypointValues),  # annotation info
-            pd.DataFrame([xy[0:3] for xy in d.xy.tolist()], columns=['dummy', 'x', 'y']),  # split xy
-            pd.DataFrame(d.z.tolist(), columns=['t']),  # time of keypoint
+            pd.DataFrame([xy[0:3] for xy in d.xy.tolist() if xz], columns=['dummy', 'x', 'y']),  # split xy
+            pd.DataFrame([z[0:1] for z in d.z.tolist() if z], columns=['t']),  # time of keypoint
         ]
-        d = d.join(DFS).drop(columns=['fid', 'xy', 'av', 'dummy', 'vid', 'z', 'spatial'])\
-             .dropna(how='all', subset=['fire', 'exploitable'])\
+        d = d.join(DFS).drop(columns=['fid', 'xy', 'av', 'dummy', 'vid', 'z', 'spatial'], errors='ignore')
+        #Adding columns to match standards csv files. Relevant if file only contains unexploitables videos
+        if 'fire' not in d:
+            d.insert(6,'fire',float('nan'))
+            d.insert(7,'sequence',float('nan'))
+            d.insert(8,'clf_confidence',float('nan'))
+            d.insert(9,'loc_confidence',float('nan'))
+
+        d = d.dropna(how='all', subset=['fire', 'exploitable'])\
              .fillna({'exploitable': True}).sort_values(['fname', 't'])
 
         # Convert time to frame
-        d['frame'] = np.round(d.fps * d.t) + d.splitStart.fillna(0)
+        d['frame'] = np.round(d.fps * d.t.fillna(0)) + d.splitStart.fillna(0)
 
         # Reject invalid keypoints
         # FIXME: frame == splitEnd can remove end of sequence.
